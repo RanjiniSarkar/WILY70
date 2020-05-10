db4e04f83b094bcb17d0017f30be66be4ad53d6d
RanjiniSarkar <ranjinisarkar07@gmail.com>
2020-05-10 20:53:06 +0530

    WILY

--- App.js
+++ App.js
@@ -1,18 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import TransactionScreen from './screens/bookTransaction';
+import SearchScreen from './screens/searchTransaction';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: {screen: TransactionScreen},
+  Search: {screen: SearchScreen},
+},
+{
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:({})=>{
+const routeName=navigation.state.routeName
+if (routeName === 'Transaction'){
+  return(
+    <Image 
+    source = {require('./assets/book.png')}
+    style = {{width:40,height:40}}
+    />
+  )
+}
+else if (routeName === 'Search'){
+  return(
+    <Image 
+    source ={require('./assets/searchingbook.png')}
+    style = {{width:40,height:40}}
+    />
+  )
+}
+
+    }
+  })
+}
+);
+
+const AppContainer =  createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
